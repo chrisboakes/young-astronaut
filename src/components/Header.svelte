de32ff16d9c20773fba292603e4f3e083a9a1835
Chris Boakes <chris@chrisboakes.com>
2020-04-18 17:32:36 +0100

    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,93 @@
+<script>
+	export let segment;
+</script>
+
+<header>
+	<h1>Young Astronaut</h1>
+</header>
+
+<h2>An indie band from the south of England</h2>
+
+<p></p>
+
+<ul class="social__items-wrap">
+	<li class="social__item">
+		<a href="https://www.facebook.com/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Facebook page">
+			<span class="u-visually-hidden">
+				Facebook
+			</span>
+			<svg class="social__svg">
+				<use xlink:href='#shape-facebook'></use></svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://twitter.com/yastronaut" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Twitter page">
+				<span class="u-visually-hidden">
+					Twitter
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-twitter'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="http://instagram.com/youngastronautband/" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Instagram page">
+				<span class="u-visually-hidden">
+					Instagram
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-instagram'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://www.youtube.com/user/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut YouTube page">
+				<span class="u-visually-hidden">
+					YouTube
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-youtube'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://soundcloud.com/young-astronaut/" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Soundcloud">
+				<span class="u-visually-hidden">
+					Soundcloud
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-soundcloud'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="http://open.spotify.com/artist/5mWvB7w84pPL7h2xXWuxaQ" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Spotify">
+				<span class="u-visually-hidden">
+					Spotify
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-spotify'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://itunes.apple.com/gb/artist/young-astronaut/id489444596" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Apple Music">
+				<span class="u-visually-hidden">
+					Apple Music
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-itunes'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="mailto:youngastronautband@gmail.com" class="social__item-link" rel="noopener nofollow" aria-label="Email Young Astronaut">
+				<span class="u-visually-hidden">
+					Email
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-email'></use>
+				</svg>
+			</a>
+		</li>
+	</ul>
---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+	<title>Young Astronaut Band</title>
+</svelte:head>
