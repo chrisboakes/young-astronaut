43b61dd241f246e80d34e9a4139a1456201d12e7
Chris Boakes <chris@chrisboakes.com>
2020-04-18 18:41:27 +0100

    Recordings

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,4 @@
 <style>
-	ul {
-		display: none;
-	}
-
 	header {
 		display: block;
 		padding: 40px 20px 20px;
@@ -15,10 +11,45 @@
 		width: 150px;
 	}
 
-	svg {
+	.logo {
 		fill: #fff;
 		height: 100%;
 		width: 100%;
+	}
+
+	.descriptions {
+		margin: 10px 0;
+	}
+
+	p {
+		color: #fff;
+		font-family: 'Courier New', Courier, monospace;
+		padding: 0;
+		text-align: center;
+	}
+
+	.social__items-wrap {
+		margin-top: 20px;
+		justify-content: center;
+		display: flex;
+	}
+
+	.social__item {
+		margin-right: 20px;
+	}
+
+	.social__item:last-child {
+		margin-right: 0;
+	}
+
+	.social__svg {
+		fill: #fff;
+		max-width: 30px;
+		height: 30px;
+	}
+
+	.social__item-link:hover .social__svg {
+		fill: #fff;
 	}
 </style>
 
@@ -26,10 +57,98 @@
 	<header>
 		<h1>
 			<span class="u-visually-hidden">Young Astronaut</span>
-			<svg>
+			<svg class="logo">
 				<title>Young Astronaut</title>
 				<use xlink:href='#shape-logo'></use>
 			</svg>
 		</h1>
 	</header>
+
+	<div class="descriptions">
+		<p>Indie band with two full length albums:<br /><em>Fawn</em> and <em>A Gold Frontier</em></p>
+		<p>New E.P. coming 2020</p>
+	</div>
+
+	<ul class="social__items-wrap">
+		<li class="social__item">
+			<a href="https://www.facebook.com/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Facebook page">
+				<span class="u-visually-hidden">
+					Facebook
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-facebook'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://twitter.com/yastronaut" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Twitter page">
+				<span class="u-visually-hidden">
+					Twitter
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-twitter'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="http://instagram.com/youngastronautband/" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Instagram page">
+				<span class="u-visually-hidden">
+					Instagram
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-instagram'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://www.youtube.com/user/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut YouTube page">
+				<span class="u-visually-hidden">
+					YouTube
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-youtube'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://soundcloud.com/young-astronaut/" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Soundcloud">
+				<span class="u-visually-hidden">
+					Soundcloud
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-soundcloud'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="http://open.spotify.com/artist/5mWvB7w84pPL7h2xXWuxaQ" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Spotify">
+				<span class="u-visually-hidden">
+					Spotify
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-spotify'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="https://itunes.apple.com/gb/artist/young-astronaut/id489444596" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Apple Music">
+				<span class="u-visually-hidden">
+					Apple Music
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-itunes'></use>
+				</svg>
+			</a>
+		</li>
+		<li class="social__item">
+			<a href="mailto:youngastronautband@gmail.com" class="social__item-link" rel="noopener nofollow" aria-label="Email Young Astronaut">
+				<span class="u-visually-hidden">
+					Email
+				</span>
+				<svg class="social__svg">
+					<use xlink:href='#shape-email'></use>
+				</svg>
+			</a>
+		</li>
+	</ul>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,59 @@
 <style>
-	ul {
-		display: none;
+	h2 {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 24px;
+		letter-spacing: 2px;
+		margin-bottom: 20px;
+		text-align: center;
+		text-transform: uppercase;
+	}
+
+	h3 {
+		color: #fff;
 	}
 
 	p {
-		color: #fff;
-		padding: 0;
-		margin: 0 0 5px 0;
-		text-align: center;
+		margin-bottom: 5px;
+	}
+
+	@media (min-width: 768px) {
+		.recordings {
+			display: flex;
+			justify-content: center;
+			margin: 20px 0 0 0;
+		}
+	}
+
+	article {
+		padding-bottom: 20px;
+		margin: 0 auto;
+		width: 300px;
+	}
+
+	@media (min-width: 768px) {
+		article {
+			margin: 0;
+		}
+
+		article:first-child {
+			margin-right: 20px;
+		}
+	}
+
+	article svg {
+		fill: #fff;
+		margin-right: 10px;
+		max-width: 20px;
+		height: 20px;
+	}
+
+	article li {
+		margin-bottom: 5px;
+	}
+
+	article li a {
+		align-items: center;
+		display: flex;
 	}
 </style>
 
@@ -16,90 +62,61 @@
 	<meta name="description" content="Music, news, videos and merchandise from British indie band Young Astronaut.">
 </svelte:head>
 
-<div>
-	<p>Indie band with two full length albums: <em>Fawn</em> and <em>A Gold Frontier</em></p>
-	<p>New E.P. coming 2020</p>
 
-	<ul class="social__items-wrap">
-		<li class="social__item">
-			<a href="https://www.facebook.com/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Facebook page">
-				<span class="u-visually-hidden">
-					Facebook
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-facebook'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="https://twitter.com/yastronaut" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Twitter page">
-				<span class="u-visually-hidden">
-					Twitter
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-twitter'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="http://instagram.com/youngastronautband/" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut Instagram page">
-				<span class="u-visually-hidden">
-					Instagram
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-instagram'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="https://www.youtube.com/user/youngastronautband" class="social__item-link" rel="noopener nofollow" aria-label="Visit the Young Astronaut YouTube page">
-				<span class="u-visually-hidden">
-					YouTube
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-youtube'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="https://soundcloud.com/young-astronaut/" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Soundcloud">
-				<span class="u-visually-hidden">
-					Soundcloud
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-soundcloud'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="http://open.spotify.com/artist/5mWvB7w84pPL7h2xXWuxaQ" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Spotify">
-				<span class="u-visually-hidden">
-					Spotify
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-spotify'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="https://itunes.apple.com/gb/artist/young-astronaut/id489444596" class="social__item-link" rel="noopener nofollow" aria-label="Listen to Young Astronaut on Apple Music">
-				<span class="u-visually-hidden">
-					Apple Music
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-itunes'></use>
-				</svg>
-			</a>
-		</li>
-		<li class="social__item">
-			<a href="mailto:youngastronautband@gmail.com" class="social__item-link" rel="noopener nofollow" aria-label="Email Young Astronaut">
-				<span class="u-visually-hidden">
-					Email
-				</span>
-				<svg class="social__svg">
-					<use xlink:href='#shape-email'></use>
-				</svg>
-			</a>
-		</li>
-	</ul>
+<div class="recordings">
+	<article>
+		<img src="/young-astronaut-a-gold-frontier.jpg" alt="Young Astronaut - A Gold Frontier album cover" />
+		<header>
+			<h3>A Gold Frontier</h3>
+			<p>Listen on:</p>
+			<ul>
+				<li>
+					<a href="https://open.spotify.com/album/27t2gkNPkfcQqUa5kCLCnI" rel="noopener nofollow" aria-label="Listen to A Gold Frontier on Spotify">
+						<svg>
+							<use xlink:href='#shape-spotify'></use>
+						</svg>
+						<span>Spotify</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://music.apple.com/gb/album/a-gold-frontier/1217721159" rel="noopener nofollow" aria-label="Listen to A Gold Frontier on Spotify">
+						<svg>
+							<use xlink:href='#shape-itunes'></use>
+						</svg>
+						<span>Apple Music</span>
+					</a>
+				</li>
+			</ul>
+		</header>
+	</article>
+
+	<article>
+		<img src="/young-astronaut-fawn.jpg" alt="Young Astronaut - Fawn album cover" />
+		<header>
+			<h3>Fawn</h3>
+			<p>Listen on:</p>
+			<ul>
+				<li>
+					<a href="https://open.spotify.com/album/38Bbfb5UWKJOVbudjIrirE" rel="noopener nofollow" aria-label="Listen to Fawn on Spotify">
+						<svg>
+							<use xlink:href='#shape-spotify'></use>
+						</svg>
+						<span>Spotify</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://music.apple.com/jp/album/fawn/489444594" rel="noopener nofollow" aria-label="Listen to Fawn on Spotify">
+						<svg>
+							<use xlink:href='#shape-itunes'></use>
+						</svg>
+						<span>Apple Music</span>
+					</a>
+				</li>
+			</ul>
+		</header>
+	</article>
 </div>
+
+<section>
+	<h2>Music Videos</h2>
+</section>
