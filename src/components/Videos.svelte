fa6f6cf0c53e730cc51b9b6e46fbbeb59572fdfc
Chris Boakes <chris@chrisboakes.com>
2020-04-19 12:07:42 +0100

    Videos component

---
+++ src/components/Videos.svelte
@@ -0,0 +1,84 @@
+<style>
+    section {
+        padding: 0 0 30px 0;
+        margin: 0 auto;
+        width: 350px;
+    }
+
+    @media (min-width: 768px) {
+        section {
+            width: 620px;
+        }
+	}
+
+    h2 {
+        color: #fff;
+        margin-bottom: 20px;
+        margin-top: 20px;
+        text-align: center;
+    }
+
+    h3 {
+        margin-bottom: 10px;
+    }
+
+    .video-wrapper {
+        overflow: visible;
+        margin-bottom: 20px;
+        padding-top: 56.25%;
+        position: relative;
+    }
+
+    .video-wrapper--thin {
+        padding-top: 42.5%;
+    }
+
+    iframe {
+        border: 0;
+        height: 100%;
+        left: 0;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+</style>
+
+<section>
+	<h2>Videos</h2>
+
+    <h3>Chasing El Dorado</h3>
+
+    <div class="video-wrapper video-wrapper--thin">
+	    <iframe title="Chasing El Dorado" width="620" height="264" src="https://www.youtube.com/embed/1KVJnExCTy8?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <h3>The Old Cool</h3>
+
+    <div class="video-wrapper video-wrapper--thin">
+	    <iframe title="The Old Cool" width="620" height="264" src="https://www.youtube.com/embed/lQabr1h4P-I?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <h3>The Chronicles Of Fall &amp; Glory</h3>
+
+    <div class="video-wrapper">        
+	    <iframe title="The Chronicles Of Fall &amp; Glory" width="620" height="264" src="https://www.youtube.com/embed/dBkLQu17VwM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <h3>Chasing El Dorado (Live Session)</h3>
+
+    <div class="video-wrapper">
+	    <iframe title="Chasing El Dorado (Live Session)" width="620" height="264" src="https://www.youtube.com/embed/5ejd-4B6FLw?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <h3>Hey Little Ghost</h3>
+
+    <div class="video-wrapper">
+	    <iframe title="Hey Little Ghost" width="620" height="264" src="https://www.youtube.com/embed/Bq4HC_RX8-o?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <h3>Triumph</h3>
+
+    <div class="video-wrapper">
+	    <iframe title="Triumph" width="620" height="264" src="https://www.youtube.com/embed/FmWVhjfsLDM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,122 +1,13 @@
-<style>
-	h2 {
-		font-family: 'Courier New', Courier, monospace;
-		font-size: 24px;
-		letter-spacing: 2px;
-		margin-bottom: 20px;
-		text-align: center;
-		text-transform: uppercase;
-	}
-
-	h3 {
-		color: #fff;
-	}
-
-	p {
-		margin-bottom: 5px;
-	}
-
-	@media (min-width: 768px) {
-		.recordings {
-			display: flex;
-			justify-content: center;
-			margin: 20px 0 0 0;
-		}
-	}
-
-	article {
-		padding-bottom: 20px;
-		margin: 0 auto;
-		width: 300px;
-	}
-
-	@media (min-width: 768px) {
-		article {
-			margin: 0;
-		}
-
-		article:first-child {
-			margin-right: 20px;
-		}
-	}
-
-	article svg {
-		fill: #fff;
-		margin-right: 10px;
-		max-width: 20px;
-		height: 20px;
-	}
-
-	article li {
-		margin-bottom: 5px;
-	}
-
-	article li a {
-		align-items: center;
-		display: flex;
-	}
-</style>
+<script>
+	import Recordings from '../components/Recordings.svelte';
+	import Videos from '../components/Videos.svelte';
+</script>
 
 <svelte:head>
 	<title>Young Astronaut Band</title>
 	<meta name="description" content="Music, news, videos and merchandise from British indie band Young Astronaut.">
 </svelte:head>
 
+<Recordings />
 
-<div class="recordings">
-	<article>
-		<img src="/young-astronaut-a-gold-frontier.jpg" alt="Young Astronaut - A Gold Frontier album cover" />
-		<header>
-			<h3>A Gold Frontier</h3>
-			<p>Listen on:</p>
-			<ul>
-				<li>
-					<a href="https://open.spotify.com/album/27t2gkNPkfcQqUa5kCLCnI" rel="noopener nofollow" aria-label="Listen to A Gold Frontier on Spotify">
-						<svg>
-							<use xlink:href='#shape-spotify'></use>
-						</svg>
-						<span>Spotify</span>
-					</a>
-				</li>
-				<li>
-					<a href="https://music.apple.com/gb/album/a-gold-frontier/1217721159" rel="noopener nofollow" aria-label="Listen to A Gold Frontier on Spotify">
-						<svg>
-							<use xlink:href='#shape-itunes'></use>
-						</svg>
-						<span>Apple Music</span>
-					</a>
-				</li>
-			</ul>
-		</header>
-	</article>
-
-	<article>
-		<img src="/young-astronaut-fawn.jpg" alt="Young Astronaut - Fawn album cover" />
-		<header>
-			<h3>Fawn</h3>
-			<p>Listen on:</p>
-			<ul>
-				<li>
-					<a href="https://open.spotify.com/album/38Bbfb5UWKJOVbudjIrirE" rel="noopener nofollow" aria-label="Listen to Fawn on Spotify">
-						<svg>
-							<use xlink:href='#shape-spotify'></use>
-						</svg>
-						<span>Spotify</span>
-					</a>
-				</li>
-				<li>
-					<a href="https://music.apple.com/jp/album/fawn/489444594" rel="noopener nofollow" aria-label="Listen to Fawn on Spotify">
-						<svg>
-							<use xlink:href='#shape-itunes'></use>
-						</svg>
-						<span>Apple Music</span>
-					</a>
-				</li>
-			</ul>
-		</header>
-	</article>
-</div>
-
-<section>
-	<h2>Music Videos</h2>
-</section>
+<Videos />
