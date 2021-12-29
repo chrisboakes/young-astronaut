d3255d0c0e52bebddb311f5fc35c103f80e42532
Chris Boakes <chris@chrisboakes.com>
2021-12-29 21:11:02 +0000

    OG & Twitter Images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,18 @@
 
 <svelte:head>
 	<title>Young Astronaut Band</title>
-	<meta name="description" content="Music, news, videos and merchandise from British indie band Young Astronaut.">
+	<meta name="description" content="Official website for British indie band Young Astronaut. Featuring music, videos and information.">
+	<meta property="og:title" content="Young Astronaut - Official Website">
+	<meta property="og:description" content="Official website for British indie band Young Astronaut. Featuring music, videos and information.">
+	<meta property="og:url" content="https://youngastronaut.co.uk/">
+	<meta property="og:site_name" content="Young Astronaut">
+	<meta property="og:image" content="https://youngastronaut.co.uk/og-image.jpg">
+	<meta content="summary" name=twitter:card>
+	<meta name="twitter:title" content="Young Astronaut - Official Website">
+	<meta name="twitter:description" content="Official website for British indie band Young Astronaut. Featuring music, videos and information.">
+	<meta name="twitter:domain" content="youngastronaut.co.uk">
+	<meta name="twitter:image:src" content="https://youngastronaut.co.uk/twitter-image.jpg">
+	<meta name="twitter:image:alt" content="Young Astronaut Band Photo">
 </svelte:head>
 
 <Recordings />
